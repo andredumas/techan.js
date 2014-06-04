e1a35079b0332ab47ed96093efa6d6ff867a96b8
André Dumas <agdumas@gmail.com>
2014-06-04 14:46:11 +1000

    adding some d3 integration tests

--- test/spec/scale/financetimeSpec.js
+++ test/spec/scale/financetimeSpec.js
@@ -1,15 +1,39 @@
 describe('techan.scale.financetime', function() {
   'use strict';
 
-  var mockLinear = function() {};
-  var mockOrdinal = function() {};
-  var financetime = require('../../../src/scale/financetime')(mockLinear, mockOrdinal);
+  describe('with mocked dependencies', function() {
+    var linear = function() {};
+    var ordinal = function() {};
+    var financetime = require('../../../src/scale/financetime')(linear, ordinal);
 
-  it('.financetime should be defined', function() {
-    expect(financetime).toBeDefined();
+    it('.financetime should be defined', function() {
+      expect(financetime).toBeDefined();
+    });
+
+    it('should be able to be constructed', function() {
+      var value = financetime();
+    });
   });
 
-  it('should be able to be constructed', function() {
-    var value = financetime();
+  describe('with actual dependencies', function() {
+    var linear = d3.scale.linear;
+    var ordinal = d3.scale.ordinal;
+    var financetime = require('../../../src/scale/financetime')(linear, ordinal);
+
+    it('.financetime should be defined', function() {
+      expect(financetime).toBeDefined();
+    });
+
+    describe('and constructed', function() {
+      var scale = null;
+
+      beforeEach(function() {
+        scale = financetime();
+      });
+
+      it('should be defined', function() {
+        expect(scale).toBeDefined();
+      });
+    });
   });
 });
