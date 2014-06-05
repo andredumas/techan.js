fa3b849696691c238c910eddf1954d1afc091a6d
André Dumas <agdumas@gmail.com>
2014-06-06 00:09:45 +1000

    WIP finance time implementation and testing

--- src/scale/financetime.js
+++ src/scale/financetime.js
@@ -11,14 +11,12 @@ module.exports = function(d3_scale_linear, d3_scale_ordinal) {
 
     this.linear = linear || d3_scale_linear();
     this.ordinal = ordinal || d3_scale_ordinal();
-
-    return function(g) {
-      // return the function to draw supstance
-    };
   }
 
   FinanceTime.prototype.invert = function(y) {
-    return this.ordinal.domain[this.linear.invert(y)];
+    var domain = this.ordinal.domain(),
+        index = Math.min(domain.length-1, Math.max(0, this.linear.invert(y)));
+    return domain[index];
   };
 
   FinanceTime.prototype.linear = function() {
@@ -32,18 +30,19 @@ module.exports = function(d3_scale_linear, d3_scale_ordinal) {
   FinanceTime.prototype.domain = function(domain) {
     if(!arguments.length) return this.ordinal.domain();
     this.linear.domain([0, domain.length]);
-    return this.ordinal.domain(domain);
+    this.ordinal.domain(domain);
+    return this;
   };
 
   FinanceTime.prototype.range = function(range) {
     if (!arguments.length) return this.linear.range();
     this.linear.range(range);
-    this.rangeRoundBands([this.linear(0), this.linear(this.domain().length)], 0.2);
-    return this.linear.range();
+    this.ordinal.rangeRoundBands([this.linear(0), this.linear(this.domain().length)], 0.2);
+    return this;
   };
 
   FinanceTime.prototype.copy = function() {
-    return new FinanceTime(this.linear, this.ordinal);
+    return new FinanceTime(this.linear.copy(), this.ordinal.copy());
   };
 
   return FinanceTime;
--- test/spec/bundle/scale/financetimeSpec.js
+++ test/spec/bundle/scale/financetimeSpec.js
@@ -15,8 +15,140 @@ techanModule('scale/financetime', function(specBuilder) {
     return module(linear, ordinal);
   };
 
+  var data = ohlc.facebook.slice(0, 10).map(function(d) { return new Date(d[0]); });
+  console.log(data);
+
   specBuilder.require(require('../../../../src/scale/financetime'), function(instanceBuilder) {
     instanceBuilder.instance('mocked', mockInit);
-    instanceBuilder.instance('actual', actualInit);
+    instanceBuilder.instance('actual', actualInit, function(bucket) {
+      var financetime = null;
+
+      describe('And domain and range is initialised', function() {
+        beforeEach(function() {
+          financetime = bucket.financetime;
+          financetime.domain(data).range([100, 110]);
+        });
+
+        it('Then domain should equal the domain set', function() {
+          expect(financetime.domain()).toEqual(data);
+        });
+
+        it('Then range should return the range set', function() {
+          expect(financetime.range()).toEqual([100, 110]);
+        });
+
+        xit('Then scale of first index should return min range', function() {
+          expect(financetime(data[0])).toEqual(100);
+        });
+
+        /*
+          Possibly convert from prototype to closure style object to support this
+         */
+        xit('Then scale of last index should return max range', function() {
+          // TODO Module is not function itself
+          expect(financetime(data[data.length-1])).toEqual(110);
+        });
+
+        it('Then invert of min range should return first domain', function() {
+          // TODO Module is not function itself
+          expect(financetime.invert(100)).toEqual(data[0]);
+        });
+
+        it('Then invert of max range should return last domain', function() {
+          expect(financetime.invert(110)).toEqual(data[data.length-1]);
+        });
+
+        it('Then invert of value before range, should return first domain', function() {
+          expect(financetime.invert(50)).toEqual(data[0]);
+        });
+
+        it('Then invert of value after range, should return last domain', function() {
+          expect(financetime.invert(150)).toEqual(data[data.length-1]);
+        });
+
+        /*
+          Under the bonnet (in zoom) d3 uses Array.map(scale.invert), which doesn't work to nice
+          with 'this' (map passes undefined), which then in turn does not play nice with prototype.
+          Might need to convert prototype to closure style objects.
+         */
+        xit('Then using invert as map function to map an array of a value greater than max range, should return last domain', function() {
+          expect([150].map(financetime.invert)).toEqual([data[data.length-1]]);
+        });
+
+        describe('And copied', function() {
+          var cloned = null;
+
+          beforeEach(function() {
+            cloned = financetime.copy();
+          });
+
+          it('Then should not be equal to source', function() {
+            expect(cloned).not.toEqual(financetime);
+          });
+
+          it('Then domain should equal the domain set', function() {
+            expect(cloned.domain()).toEqual(data);
+          });
+
+          it('Then range should return the range set', function() {
+            expect(cloned.range()).toEqual([100, 110]);
+          });
+        });
+
+        describe('And a zoom applied', function() {
+          var zoom = null;
+
+          beforeEach(function() {
+            zoom = d3.behavior.zoom();
+            financetime = bucket.financetime;
+            zoom.x(financetime);
+          });
+
+          describe('And translated by 50 left', function() {
+            beforeEach(function() {
+              zoom.translate([-50, 0]);
+            });
+
+            xit('Then scale of first index should return min range', function() {
+              expect(financetime(data[0])).toEqual(50);
+            });
+
+            xit('Then scale of last index should return max range', function() {
+              expect(financetime(data[data.length-1])).toEqual(60);
+            });
+
+            xit('Then invert of min range should return first domain', function() {
+              expect(financetime.invert(50)).toEqual(data[0]);
+            });
+
+            xit('Then invert of max range should return last domain', function() {
+              expect(financetime.invert(60)).toEqual(data[data.length-1]);
+            });
+          });
+
+          describe('And scaled by 2', function() {
+            beforeEach(function() {
+              zoom.scale(2);
+            });
+
+            xit('Then scale of first index should return min range', function() {
+              expect(financetime(data[0])).toEqual(50);
+            });
+
+            xit('Then scale of last index should return max range', function() {
+              expect(financetime(data[data.length-1])).toEqual(140);
+            });
+
+            xit('Then invert of min range should return first domain', function() {
+              expect(financetime.invert(50)).toEqual(data[0]);
+            });
+
+            xit('Then invert of max range should return last domain', function() {
+              expect(financetime.invert(140)).toEqual(data[data.length-1]);
+            });
+          });
+        });
+      });
+    });
   });
 });
