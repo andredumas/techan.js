e1cdf442175f5435ef2c58ef7e2653e930a922c3
André Dumas <agdumas@gmail.com>
2014-07-14 01:17:51 +1000

    Use default accessor when one not passed

--- src/scale/index.js
+++ src/scale/index.js
@@ -3,6 +3,7 @@
 module.exports = function(d3) {
   var zoomable = require('./zoomable')(),
       util = require('../util')(),
+      accessors = require('../accessor')(),
       financetime = require('./financetime')(d3.scale.linear, d3.time, d3.bisect, zoomable, util.rebindCallback);
 
   return {
@@ -20,48 +21,52 @@ module.exports = function(d3) {
 
     plot: {
       time: function(data, accessor) {
+        accessor = accessor || accessors.value();
         return financetime().domain(data.map(accessor.d));
       },
 
       percent: function (scale, reference) {
         var domain = scale.domain();
+        reference = reference || domain[0];
         return scale.copy().domain([((domain[0] - reference) / reference), ((domain[1] - reference) / reference)]);
       },
 
       ohlc: function (data, accessor) {
+        accessor = accessor || accessors.ohlc();
         return d3.scale.linear()
           .domain([d3.min(data.map(accessor.low())) * 0.98, d3.max(data.map(accessor.high())) * 1.03])
           .range([1, 0]);
       },
 
       volume: function (data, accessor) {
+        accessor = accessor || accessors.ohlc().v;
         return d3.scale.linear()
           .domain([0, d3.max(data.map(accessor)) * 1.15])
           .range([1, 0]);
       },
 
-      rsi: function (data, accessor) {
+      rsi: function () {
         return d3.scale.linear().domain([0, 100])
           .range([1, 0]);
       },
 
-      path: function(data, accessor) {
-        return pathScale(d3, data, accessor);
-      },
-
       momentum: function(data, accessor) {
+        accessor = accessor || accessors.value();
         return pathScale(d3, data, accessor);
       },
 
       moneyflow: function(data, accessor) {
+        accessor = accessor || accessors.value();
         return pathScale(d3, data, accessor);
       },
 
       macd: function(data, accessor) {
+        accessor = accessor || accessors.macd();
         return pathScale(d3, data, accessor);
       },
 
       movingaverage: function(data, accessor) {
+        accessor = accessor || accessors.value();
         return pathScale(d3, data, accessor);
       }
     },
--- test/spec/bundle/scale/indexSpec.js
+++ test/spec/bundle/scale/indexSpec.js
@@ -1,9 +1,27 @@
 techanModule('scale', function(specBuilder) {
   'use strict';
 
+  var techan = require('../../../../src/techan');
+
   var actualInit = function(module) {
     return module(d3);
   };
+
+  var data = require('../_fixtures/data/ohlc').facebook.slice(0, 10).map(function(d) { return d.date; });
+
+  function optionalAccessorTest(name, object, accessor) {
+    it('Then ' + name + ' should be defined', function() {
+      expect(object()).toBeDefined();
+    });
+
+    it('Then ' + name + ' without accessor, should be invoked without error', function() {
+      expect(object()(data)).toBeDefined();
+    });
+
+    it('Then ' + name + ' with accessor, should be invoked without error', function() {
+      expect(object()(data, accessor)).toBeDefined();
+    });
+  }
 
   specBuilder.require(require('../../../../src/scale'), function(instanceBuilder) {
     instanceBuilder.index('actual', actualInit, function(scope) {
@@ -11,47 +29,65 @@ techanModule('scale', function(specBuilder) {
         expect(scope.scale.financetime).toBeDefined();
       });
 
-      it('Then scale.plot.time should be defined', function() {
-        expect(scope.scale.plot.time).toBeDefined();
-      });
+      optionalAccessorTest('scale.plot.time', function() {
+        return scope.scale.plot.time;
+      }, techan.accessor.ohlc());
+
+      optionalAccessorTest('scale.plot.ohlc', function() {
+        return scope.scale.plot.ohlc;
+      }, techan.accessor.ohlc());
+
+      optionalAccessorTest('scale.plot.volume', function() {
+        return scope.scale.plot.volume;
+      }, techan.accessor.ohlc().v);
+
+      optionalAccessorTest('scale.plot.rsi', function() {
+        return scope.scale.plot.rsi;
+      }, techan.accessor.rsi());
+
+      optionalAccessorTest('scale.plot.momentum', function() {
+        return scope.scale.plot.momentum;
+      }, techan.accessor.value());
+
+      optionalAccessorTest('scale.plot.moneyflow', function() {
+        return scope.scale.plot.moneyflow;
+      }, techan.accessor.value());
+
+      optionalAccessorTest('scale.plot.macd', function() {
+        return scope.scale.plot.macd;
+      }, techan.accessor.macd());
+
+      optionalAccessorTest('scale.plot.movingaverage', function() {
+        return scope.scale.plot.movingaverage;
+      }, techan.accessor.value());
 
       it('Then scale.plot.percent should be defined', function() {
         expect(scope.scale.plot.percent).toBeDefined();
       });
 
-      it('Then scale.plot.ohlc should be defined', function() {
-        expect(scope.scale.plot.ohlc).toBeDefined();
+      describe('And initialise the percent scale using default reference', function() {
+        var y,
+          scale;
+
+        beforeEach(function() {
+          y = d3.scale.linear().domain([100, 200]).range([100, 0]);
+          scale = scope.scale.plot.percent(y);
+        });
+
+        it('Then the inverted domain reference value should be 0%', function() {
+          expect(scale.invert(y(100))).toEqual(0);
+        });
+
+        it('Then the inverted 80% of reference domain value should be -20%', function() {
+          expect(scale.invert(y(80))).toEqual(-0.2);
+        });
+
+        it('Then the inverted max domain value should be 100%', function() {
+          expect(scale.invert(y(200))).toEqual(1);
+        });
       });
 
-      it('Then scale.plot.volume should be defined', function() {
-        expect(scope.scale.plot.volume).toBeDefined();
-      });
-
-      it('Then scale.plot.rsi should be defined', function() {
-        expect(scope.scale.plot.rsi).toBeDefined();
-      });
-
-      it('Then scale.plot.path should be defined', function() {
-        expect(scope.scale.plot.path).toBeDefined();
-      });
-
-      it('Then scale.plot.momentum should be defined', function() {
-        expect(scope.scale.plot.momentum).toBeDefined();
-      });
-
-      it('Then scale.plot.moneyflow should be defined', function() {
-        expect(scope.scale.plot.moneyflow).toBeDefined();
-      });
-
-      it('Then scale.plot.macd should be defined', function() {
-        expect(scope.scale.plot.macd).toBeDefined();
-      });
-
-      it('Then scale.plot.movingaverage should be defined', function() {
-        expect(scope.scale.plot.movingaverage).toBeDefined();
-      });
-
-      describe('And initialise the percent scale from a base scale', function() {
+      describe('And initialise the percent scale with reference value', function() {
         var y,
             scale;
 
