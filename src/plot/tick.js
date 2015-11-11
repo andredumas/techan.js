18a5d558e86899f1f2acdc876935ed42f83c7d2e
Thomas Han <thomas.han@live.com>
2015-11-11 23:38:30 +1100

    group up, down, equal on ticks

--- src/plot/tick.js
+++ src/plot/tick.js
@@ -7,8 +7,10 @@ module.exports = function(d3_scale_linear, d3_extent, accessor_tick, plot, plotM
         lineWidthGenerator;
 
     function tick(g) {
+      var upDownEqual = groupUpDownEqual(g.datum(), p.accessor);
       var group = plot.groupSelect(g, plot.dataMapper.array, p.accessor.d);
-      group.entry.append('path').attr('class', 'tick');
+
+      plot.appendUpDownEqual(group.selection, p.accessor, ['tick'], upDownEqual);
 
       tick.refresh(g);
     }
@@ -28,6 +30,41 @@ module.exports = function(d3_scale_linear, d3_extent, accessor_tick, plot, plotM
     return tick;
   };
 };
+
+function groupUpDownEqual(data, accessor) {
+  return data.reduce(function(result, current, index, array) {
+    var previous = getPrevious(array, index);
+    if (up(accessor, previous, current)) result.up.push(current);
+    else if (down(accessor, previous, current)) result.down.push(current);
+    else result.equal.push(current);
+    return result;
+  }, { up: [], down: [], equal: [] });
+}
+
+function getPrevious(array, index) {
+  if(array && index > 0) {
+    return array[index-1];
+  }
+
+  return nullTick();
+}
+
+function nullTick() {
+  return {
+    date: null,
+    high: null,
+    low: null,
+    spread: null
+  };
+}
+
+function up(accessor, previous, current) {
+  return accessor.h(previous) < accessor.h(current) && accessor.l(previous) < accessor.l(current);
+}
+
+function down(accessor, previous, current) {
+  return accessor.h(previous) > accessor.h(current) && accessor.l(previous) > accessor.l(current);
+}
 
 function tickPath(accessor, x, y, barWidth) {
   return function(d) {
