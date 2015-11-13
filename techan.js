ecce677507cd06e1f09d3e1ed8f7e34ce5c4c92f
André Dumas <agdumas@gmail.com>
2015-11-14 00:24:35 +1100

    Update to 0.6.0-5

--- techan.js
+++ techan.js
@@ -1,9 +1,9 @@
 /*
- TechanJS v0.6.0-4
+ TechanJS v0.6.0-5
  (c) 2014 - 2015 Andre Dumas | https://github.com/andredumas/techan.js
 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.techan = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';module.exports='0.6.0-4';
+'use strict';module.exports='0.6.0-5';
 },{}],2:[function(require,module,exports){
 'use strict';
 
@@ -1202,7 +1202,7 @@ function backgroundPath(accessor, axis, height, width, point, neg) {
 
         return [
           'M', 0, value,
-          'l', neg*axis.innerTickSize(), -pt,
+          'l', neg*Math.max(axis.innerTickSize(), 1), -pt,
           'l', 0, -h,
           'l', neg*width, 0,
           'l', 0, height,
@@ -1218,7 +1218,7 @@ function backgroundPath(accessor, axis, height, width, point, neg) {
 
         return [
           'M', value, 0,
-          'l', -pt, neg*axis.innerTickSize(),
+          'l', -pt, neg*Math.max(axis.innerTickSize(), 1),
           'l', -w, 0,
           'l', 0, neg*height,
           'l', width, 0,
